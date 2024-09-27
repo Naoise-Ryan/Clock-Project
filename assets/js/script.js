43ec13f889cf76df5ae4073a7737792b47f037fc
Naoise <c00281427@setu.ie>
2024-09-27 19:28:33 +0100

    Finished minutes and hour falling blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,9 +12,9 @@ let minutes = 0
 let hours = 0
 
 //Data setup = X, Y, time Type, SizeX, SizeY, Color, row length, loops
-const secondsData = [canvas.width - 280, 300, seconds, 25, 25, "red", 10, 0]
-const minutesData = [515, 300, minutes, 25, 25, "blue", 10, 0]
-const hoursData = [30, 300, hours, 62.5, 25, "green", 4, 0]
+const secondsData = [canvas.width - 280, 400, seconds, 25, 25, "red", 10, 0]
+const minutesData = [515, 400, minutes, 25, 25, "blue", 10, 0]
+const hoursData = [30, 400, hours, 62.5, 25, "green", 4, 0]
 
 function codeLoop() {
     updateTime()
@@ -24,6 +24,14 @@ function codeLoop() {
 
 function returnSeconds(){
     return new Date().getSeconds()
+}
+
+function returnMinutes(){
+    return new Date().getMinutes()
+}
+
+function returnHours(){
+    return new Date().getHours()
 }
 
 function updateTime() {
@@ -93,20 +101,24 @@ function drawNextCube(xPosMultiplyer, timeArray){
     //yPos = secondsData[1] - 300 * (1 - milisecs/(1000 + (118 * secondsData[7])))
     yPos = makeNextCubeFall(timeArray)
 
-    context.clearRect(timeArray[0]-1, yPos-2, 350, 30);
+    context.clearRect(timeArray[0]-1, yPos-1, 350, 27);
     drawSquare(xPos, yPos, timeArray[3], timeArray[4], timeArray[5])
 }
 
 function makeNextCubeFall(timeArray){
     let newYPos = timeArray[1] - 275
     if (timeArray == secondsData){
-        newYPos = timeArray[1] - 300 * (1 - milisecs/(1000 + (118 * timeArray[7])))
+        //newYPos = timeArray[1] - 300 * (1 - milisecs/(1000 + (118 * timeArray[7])))
+       newYPos = timeArray[1] - 295 * (1 - milisecs/(1000 + (118 * timeArray[7])))
     }
     else if (timeArray == minutesData) {
-
+        //newYPos = timeArray[1] - 500 * (0.6 - seconds/100)
+        newYPos = timeArray[1] - 400 * ((0.6 + timeArray[7] / 20) - seconds/(100))
     }
     else if (timeArray == hoursData) {
-
+        newYPos = timeArray[1] - 300 * ((0.6 + timeArray[7] / 11.7647) - (minutes/100))
+        //Y is the base level of the first row, which is minused by 300 to have it 300 higher, A number from 0 - 1 is then calculated and then used
+        //To multiply the 300 to gradually lower it by a % amount so that at 1 minute it is at the top and at 60 minutes its at the bottom
     }
 
     return newYPos
@@ -114,12 +126,21 @@ function makeNextCubeFall(timeArray){
 
 
 function callEverySecond(){
-    returnSeconds()
-
     secondsData[7] = Math.floor(returnSeconds() / 10)
 }
 
+function callEveryMinute(){
+    minutesData[7] = Math.floor(returnMinutes() / 10)
+}
 
-setInterval(callEverySecond, 1000)
+function callEveryHour(){
+    hoursData[7] = Math.floor(returnHours() / 4)
+}
+
+setInterval(callEverySecond, 100)
+
+setInterval(callEveryMinute, 100)
+
+setInterval(callEveryHour, 100)
 
 codeLoop()
